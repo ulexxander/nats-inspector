92db6e56337e9301374d6eea582564f300648775
Alexander Ustyugov <ulexxander@gmail.com>
2021-05-17 22:27:28 +0200

    shared/types.d.ts filled up with typedefs

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -1,13 +1,91 @@
-export declare type WsSubMessageEvent = {
-  type: "SUB_MESSAGE";
-  payload: {
-    id: string;
-    subject: string;
-    data: unknown;
-  };
+// database models
+
+export type ConnectionModel = {
+  id: number;
+  title: string;
+  description: string | null;
+  host: string;
+  port: number;
+  dateCreated: string;
+  dateUpdated?: string;
 };
-export declare type WsEvent = WsSubMessageEvent;
-export declare type NatsSub = {
+
+export type SubscriptionModel = {
+  id: number;
+  connectionId: number;
   subject: string;
   dateCreated: string;
 };
+
+// database queries
+
+export type InsertConnectionVars = Pick<
+  ConnectionModel,
+  "title" | "description" | "host" | "port"
+>;
+
+export type DeleteConnectionVars = Pick<ConnectionModel, "id">;
+
+export type InsertSubscriptionVars = Pick<
+  SubscriptionModel,
+  "connectionId" | "subject"
+>;
+
+export type DeleteSubscriptionVars = Pick<SubscriptionModel, "id">;
+
+// service typedefs
+
+export type PausedConnection = {
+  model: ConnectionModel;
+  error?: {
+    message: string;
+    timestamp: string;
+  };
+};
+
+export type ActiveConnection = {
+  model: ConnectionModel;
+};
+
+// restapi inputs
+
+export type SendRequestInput = {
+  connectionId: number;
+  subject: string;
+  payload?: string;
+};
+
+// restapi outputs
+
+export type SendRequestOutput = {
+  id: string;
+  response: string;
+  dateCreated: string;
+};
+
+export type ActiveConnectionsOutput = Pick<ActiveConnection, "model">[];
+export type PausedConnectionsOutput = PausedConnection[];
+
+// websocket
+
+export type WsEventDef<Type, Payload> = { t: Type; p: Payload };
+
+export type WsSubscriptionMsgEvent = WsEventDef<
+  "SUBSCRIPTION_MSG",
+  {
+    id: string;
+    subject: string;
+    data: unknown;
+  }
+>;
+
+export type WsSubscriptionErrEvent = WsEventDef<
+  "SUBSCRIPTION_ERR",
+  {
+    id: string;
+    subject: string;
+    error: string;
+  }
+>;
+
+export type WsEvent = WsSubscriptionMsgEvent | WsSubscriptionErrEvent;
